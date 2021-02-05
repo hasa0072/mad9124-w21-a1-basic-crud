941eeeb402adac46dab3f00eb67a48885adac673
Sevda Hasanli <hasa0072@algonquinlive.com>
2021-02-05 01:29:45 -0500

    CRUD for Courses Resources
    Replicated entire Students CRUD and modified it accordingly for Courses

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // load dependencies
 const express = require('express')
 const studentRouter = require('./routes/student.js')
+const courseRouter = require('./routes/course.js')
 
 // create the express app
 const app = express()
@@ -9,6 +10,7 @@ const app = express()
 // configure express middleware
 app.use(express.json())
 app.use('/api/students', studentRouter)
+app.use('/api/courses', courseRouter)
 
 // start listening for HTTP requests
 const port = process.env.port || 3030
